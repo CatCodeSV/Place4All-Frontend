dd926dea9ac6548f3b6c52ef4e887811ec8cebd7
Alexander Aguirre <keevinaguirre@gmail.com>
2023-03-06 10:49:24 +0100

    Se maqueta la card de cada restaurante

---
+++ src/views/Restaurants/RestaurantCard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref, toRef } from 'vue';
+import { defineProps } from 'vue';
+import { Restaurant } from '@/models/Restaurant';
+
+const loading = ref(false);
+const props = defineProps<{
+  restaurant: Restaurant;
+}>();
+
+function reserve() {
+  loading.value = true;
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 2000);
+}
+</script>
+
+<template>
+  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+    <template v-slot:loader="{ isActive }">
+      <v-progress-linear :active="isActive" color="secondary" height="4" indeterminate></v-progress-linear>
+    </template>
+
+    <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+
+    <v-card-item>
+      <v-card-title>{{ props.restaurant.name }}</v-card-title>
+    </v-card-item>
+
+    <v-card-text>
+      <v-row align="center" class="mx-0">
+        <v-rating
+          :model-value="props.restaurant.reviews.reviewsAverage"
+          color="amber"
+          density="compact"
+          half-increments
+          readonly
+          size="medium"></v-rating>
+
+        <div class="text-grey ms-4">{{ props.restaurant.reviews.reviewsAverage }} ({{ props.restaurant.reviews.reviewsNumber }})</div>
+      </v-row>
+
+      <div class="mt-4 text-subtitle-1">$ • Italiano, Cafe</div>
+    </v-card-text>
+
+    <v-divider class="mx-4 mb-1"></v-divider>
+
+    <v-card-actions>
+      <v-btn color="primary" variant="text" @click="reserve"> Reservar </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- src/views/Restaurants/Restaurants.vue
+++ src/views/Restaurants/Restaurants.vue
@@ -1,6 +1,14 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { useRestaurant } from '@/composables/useRestaurant';
+import { onBeforeMount, ref } from 'vue';
+import RestaurantCard from './RestaurantCard.vue';
 
+onBeforeMount(async () => {
+  loading.value = true;
+  await setRestaurants();
+  loading.value = false;
+});
+const { restaurants, setRestaurants } = useRestaurant();
 const loading = ref(false);
 const selection = ref(1);
 const rating = ref(4.5);
@@ -15,33 +23,9 @@ function reserve() {
 </script>
 
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374">
-    <template v-slot:loader="{ isActive }">
-      <v-progress-linear :active="isActive" color="secondary" height="4" indeterminate></v-progress-linear>
-    </template>
-
-    <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-    <v-card-item>
-      <v-card-title>Cafe Badilico</v-card-title>
-    </v-card-item>
-
-    <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating :model-value="rating" color="amber" density="compact" half-increments readonly size="medium"></v-rating>
-
-        <div class="text-grey ms-4">{{ rating }} (413)</div>
-      </v-row>
-
-      <div class="mt-4 text-subtitle-1">$ • Italiano, Cafe</div>
-    </v-card-text>
-
-    <v-divider class="mx-4 mb-1"></v-divider>
-
-    <v-card-actions>
-      <v-btn color="primary" variant="text" @click="reserve"> Reservar </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <RestaurantCard v-for="restaurant of restaurants" :key="restaurant.id.toString()" :restaurant="restaurant" />
+  </div>
 </template>
 
 <style scoped></style>
