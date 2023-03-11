36ce9c6d333707af56e72b7a78e65e1fd8a232cd
Laura Gil <a25589svalero.com>
2023-03-11 19:16:50 +0100

    Modificado el css de RestaurantCard

--- src/views/Restaurants/RestaurantCard.vue
+++ src/views/Restaurants/RestaurantCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Address } from '@/models/Address';
 import { Restaurant } from '@/models/Restaurant';
 import { defineProps, ref } from 'vue';
 
@@ -14,6 +15,10 @@ function reserve() {
     loading.value = false;
   }, 2000);
 }
+
+function summarizeAdress(address: Address) {
+  return `${address.street} ${address.number}, ${address.city}`;
+}
 </script>
 
 <template>
@@ -28,7 +33,7 @@ function reserve() {
       <v-card-title>{{ props.restaurant.name }}</v-card-title>
     </v-card-item>
 
-    <v-card-text>
+    <v-card-text class="v-card-text">
       <v-row align="center" class="mx-0">
         <v-rating
           :model-value="props.restaurant.reviews?.reviewsAverage"
@@ -39,17 +44,35 @@ function reserve() {
           size="medium"></v-rating>
 
         <div class="text-grey ms-4">
-          {{ props.restaurant.reviews?.reviewsAverage }} ({{ props.restaurant.reviews?.reviewsNumber }})
+          {{ props.restaurant.reviews?.reviewsAverage || 0 }}
         </div>
       </v-row>
 
-      <div class="mt-4 text-subtitle-1">$ • Italiano, Cafe</div>
+      <div class="mt-4 text-subtitle-1">{{ summarizeAdress(props.restaurant.address) }}</div>
     </v-card-text>
 
     <v-divider class="mx-4 mb-1"></v-divider>
 
-    <v-card-actions>
-      <v-btn color="primary" variant="text" @click="reserve"> Reservar </v-btn>
+    <v-card-actions class="v-card-actions">
+      <v-btn prepend-icon="mdi-calendar-clock" color="primary" variant="tonal" rounded="pill" @click="reserve" class="v-btn">
+        Reservar
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
+
+<style scoped lang="scss">
+.mx-auto {
+  border-radius: 1.5rem;
+}
+.v-card-text {
+  margin-top: 0.5rem;
+}
+.v-card-actions {
+  justify-content: center;
+  padding: 0.6rem;
+}
+.v-card-actions .v-btn {
+  padding: 0 1.5rem;
+}
+</style>
