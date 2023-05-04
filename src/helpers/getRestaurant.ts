ea35250506102f438f4d116ecc09ebe93a5c2030
Laura Gil <a25589svalero.com>
2023-05-04 20:13:53 +0200

    Creado getRestaurantQuery para que devuelva un listado de RestaurantSummarized seg'un la query en el caso de que se utilice alg'un filtro, tambi'en se ha cambiado getRestaurants para que devuelva un listado de RestaurantSummarized

--- src/helpers/getRestaurant.ts
+++ src/helpers/getRestaurant.ts
@@ -1,10 +1,11 @@
 import { apiClient } from '@/api/apiClient.m';
 import { Restaurant } from '@/models/Restaurant';
+import { RestaurantSummarized } from '@/models/RestaurantSummarized';
 
 const baseURL = '/Restaurants';
 
-async function getRestaurants(): Promise<Restaurant[]> {
-  const response = await apiClient.getAll<Restaurant>(baseURL);
+async function getRestaurants(): Promise<RestaurantSummarized[]> {
+  const response = await apiClient.getAll<RestaurantSummarized>(baseURL);
   console.log(response.data);
   return response.data;
 }
@@ -14,10 +15,15 @@ async function getRestaurant(id: number | string): Promise<Restaurant> {
   return response.data;
 }
 
+async function getRestaurantsQuery(query: string): Promise<RestaurantSummarized[]> {
+  const response = await apiClient.getAll<RestaurantSummarized>(baseURL, '?query=${query}');
+  return response.data;
+}
+
 async function updateRestaurant(id: number, restaurant: Restaurant) {
   console.log(restaurant.features);
   const response = await apiClient.putEntity<Restaurant>(baseURL, id, restaurant);
   return response.data;
 }
 
-export default { getRestaurants, getRestaurant, updateRestaurant };
+export default { getRestaurants, getRestaurant, updateRestaurant, getRestaurantsQuery };
