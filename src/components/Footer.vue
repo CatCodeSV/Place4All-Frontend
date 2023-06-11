7e679328070abc091fc6d9a1e2d6e5c7b9f967a4
Alexander Aguirre <keevinaguirre@gmail.com>
2023-06-12 00:40:47 +0200

    Correcciones de Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-footer
-    :elevation="2"
-    class="index bg-primary-300 text-center d-flex flex-column align-items-center justify-center"
-    color="secondary">
-    <div class="text-footer pa-2 my-2 font-weight-bold p-h1 text-p text-primary-800">
-      <p>©2023 Place4All by CatCode.</p>
-      <p>Todos los derechos reservados.</p>
+  <v-footer :elevation="2" class="index text-center d-flex flex-column align-items-center justify-center" color="primary">
+    <div class="mt-2 text-footer text-white pa-2 my-2 font-weight-bold p-h1 text-p text-primary-800">
+      <p class="text-white">©2023 Place4All by CatCode.</p>
     </div>
-    <div class="link-text-footer pa-3 p-h1 text-p text-primary-300">
-      <a href="">Política de cookies</a>
-      <a href="">Aviso legal</a>
-      <a href="">Política de privacidad</a>
+    <div class="link-text-footer text-primary-300 mb-4">
+      <a class="text-white mx-4" href="">Cookies</a>
+      <a class="text-white mx-4" href="">Aviso legal</a>
+      <a class="text-white mx-4" href="">Privacidad</a>
     </div>
   </v-footer>
 </template>
@@ -21,27 +17,13 @@
 .index {
   max-height: 80px;
 }
-.text-footer {
-  color: #0b3d91;
-}
-.link-text-footer a {
-  margin-left: 100px;
-  margin-right: 100px;
 
-  color: #051b40;
-}
-.link-text-footer {
-  width: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-around;
-}
 @media (max-width: 600px) {
   .link-text-footer a {
     margin-left: 30px;
     margin-right: 30px;
   }
+
   .link-text-footer {
     display: flex;
     flex-direction: column;
